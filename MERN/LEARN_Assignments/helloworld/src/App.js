6247d45b115d01dc02670e0e3eb5dbcbae22b36e
JustonSmith <75957152+JustonSmith@users.noreply.github.com>
2021-05-09 16:18:35 -0500

    propitup

---
+++ MERN/LEARN_Assignments/helloworld/src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+
+// this is a component. className instead of class.  
+function App() {
+  return (
+    <>
+    <div className="App">
+      <h1>Here are the top movies.</h1>
+    </div>
+    <div>
+      <h3>Top movies</h3>
+      <ol>
+        <li>Shawshank Redemption</li>
+        <li>Catch Me if You Can</li>
+        <li>Dont be a Menace</li>
+        <li>Harold and Kumar go to White Castle</li>
+      </ol>
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ MERN/LEARN_Assignments/propitup/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello. Welcome to Person Card.</h1>
+      <PersonCard lastname= {"Doe"} firstname= {"Jane"} age= {45} haircolor= {"Black"}></PersonCard>
+      <PersonCard lastname= {"Smith"} firstname= {"John"} age= {88} haircolor= {"Green"}></PersonCard>
+      <PersonCard lastname= {"Fillmore"} firstname= {"Millard"} age= {50} haircolor= {"Brown"}></PersonCard>
+      <PersonCard lastname= {"Smith"} firstname= {"Maria"} age= {62} haircolor= {"Blonde"}></PersonCard>
+    </div>
+  );
+}
+
+export default App;
