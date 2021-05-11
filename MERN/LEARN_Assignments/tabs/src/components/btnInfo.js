8ae20a10a71259e9319190947faaa40a01e3b36a
JustonSmith <75957152+JustonSmith@users.noreply.github.com>
2021-05-10 20:10:45 -0500

    tabs

---
+++ MERN/LEARN_Assignments/tabs/src/components/btnInfo.js
@@ -0,0 +1,19 @@
+const btnInfo = [
+    {
+        label: "Tab 1",
+        content: "Tab 1 content here",
+        color: "red",
+    },
+    {
+        label: "Tab 2",
+        content: "Tab 2 content here",
+        color: "green",
+    },
+    {
+        label: "Tab 3",
+        content: "Tab 3 content here",
+        color: "blue",
+    },
+]
+
+export default btnInfo;
