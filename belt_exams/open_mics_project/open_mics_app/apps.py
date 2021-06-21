28923fe8ff6d53714c0a95924980da7f9d7769bb
JustonSmith <75957152+JustonSmith@users.noreply.github.com>
2021-06-21 10:02:21 -0500

    resubmit

--- belt_exams/open_mics_project/open_mics_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpenMicsAppConfig(AppConfig):
-    name = 'open_mics_app'
--- belt_exams/python_exam_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/JudgeMacbook/Coding_Dojo/belt_exams/python_exam_env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
