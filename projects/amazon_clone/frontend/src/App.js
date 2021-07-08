7dc818ff0e5818bcd55c9a6547a0aca8d57eddeb
JustonSmith <75957152+JustonSmith@users.noreply.github.com>
2021-07-07 23:49:13 -0500

    pamazon

--- projects/amazon_clone/frontend/src/App.js
+++ projects/amazon_clone/frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import data from './data';
 
 function App() {
   return (
-    <div class="grid-container">
-        <header class="row">
+    <div className="grid-container">
+        <header className="row">
             <div>
-                <a class="brand" href="index.html">pamazon</a>
+                <a className="brand" href="index.html">pamazon</a>
             </div>
             <div>
                 <a href="cart.html">Cart</a>
@@ -14,118 +14,31 @@ function App() {
             </div>
         </header>
         <main>
-            <div class="row center">
-                <div class="card">
-                    <a href="product.html">
-                        <img class="medium" src="./images/13hours.jpg" alt="product" />
+            <div className="row center">
+              {data.products.map((product) => (
+                <div key = {product._id} className="card">
+                    <a href={`/product/${product._id}`}>
+                        <img className="medium" src={product.image} alt="product" />
                     </a>
                     <div>
                         <a href="product.html">
-                            <h2>13 Hours</h2>
+                            <h2>{product.name}</h2>
                         </a>
-                        <div class="rating">
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
+                        <div className="rating">
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
                         </div>
-                        <div class="price">$120</div>
+                        <div className="price">${product.price}</div>
                     </div>
                 </div>
-                <div class="card">
-                    <a href="product.html">
-                        <img class="medium" src="./images/13hours.jpg" alt="product" />
-                    </a>
-                    <div>
-                        <a href="product.html">
-                            <h2>13 Hours</h2>
-                        </a>
-                        <div class="rating">
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                        </div>
-                        <div class="price">$120</div>
-                    </div>
-                </div>
-                <div class="card">
-                    <a href="product.html">
-                        <img class="medium" src="./images/13hours.jpg" alt="product" />
-                    </a>
-                    <div>
-                        <a href="product.html">
-                            <h2>13 Hours</h2>
-                        </a>
-                        <div class="rating">
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                        </div>
-                        <div class="price">$120</div>
-                    </div>
-                </div>
-                <div class="card">
-                    <a href="product.html">
-                        <img class="medium" src="./images/13hours.jpg" alt="product" />
-                    </a>
-                    <div>
-                        <a href="product.html">
-                            <h2>13 Hours</h2>
-                        </a>
-                        <div class="rating">
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                        </div>
-                        <div class="price">$120</div>
-                    </div>
-                </div>
-                <div class="card">
-                    <a href="product.html">
-                        <img class="medium" src="./images/13hours.jpg" alt="product" />
-                    </a>
-                    <div>
-                        <a href="product.html">
-                            <h2>13 Hours</h2>
-                        </a>
-                        <div class="rating">
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                        </div>
-                        <div class="price">$120</div>
-                    </div>
-                </div>
-                <div class="card">
-                    <a href="product.html">
-                        <img class="medium" src="./images/13hours.jpg" alt="product" />
-                    </a>
-                    <div>
-                        <a href="product.html">
-                            <h2>13 Hours</h2>
-                        </a>
-                        <div class="rating">
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                        </div>
-                        <div class="price">$120</div>
-                    </div>
-                </div>
+              ))
+              }
             </div>
         </main>
-        <footer class="row center">
+        <footer className="row center">
             All Rights Reserved
         </footer>
     </div>
---
+++ projects/amazon_clone/frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'13 hours',
+            category: 'Movies',
+            image: '/images/13hours.jpg',
+            price: 20,
+            brand: 'definitely off',
+            rating:'3.5',
+            numReviews: 10,
+            description: 'Jim Halpert stars in a film that follows six members of Annex Security Team who fought to defend the American diplomatic compund in Benghazi,Libya on September 11, 2012.'
+        },
+        {
+            _id:'2',
+            name:'Seafoam Teapot',
+            category: 'Kitchen and Dining',
+            image: '/images/teapot.jpg',
+            price: 25,
+            brand: 'The Metropolitan Tea Company',
+            rating:'4.5',
+            numReviews: 7,
+            description: 'Teapot makes 2 cups of tea. Can also be filled with thoughtful memories by a secret admirer whom you will someday marry.'
+        },
+        {
+            _id:'3',
+            name:'Jelly Beans',
+            category: 'Candy and Chocolate',
+            image: '/images/jellybeans.jpg',
+            price: 10,
+            brand: 'Jelly Belly',
+            rating:'3.0',
+            numReviews: 5,
+            description: 'No one is sure what they are made of. Take one.'
+        },
+        {
+            _id:'4',
+            name:'Xerox VersaLink B7030',
+            category: 'Candy and Chocolate',
+            image: '/images/copymachine.jpg',
+            price: 2700,
+            brand: 'Xerox',
+            rating:'5.0',
+            numReviews: 15,
+            description: 'The new copy machine of your dreams.'
+        },
+        {
+            _id:'5',
+            name:'5 lb box of pens.',
+            category: 'Office Supplies',
+            image: '/images/pens.jpg',
+            price: 50,
+            brand: 'DG Collection',
+            rating:'4.0',
+            numReviews: 20,
+            description: 'Bulk assorted pens, because the ones you just bought disappeared. ...again.'
+        },
+        {
+            _id:'6',
+            name:'Deluxe artist painting set',
+            category: 'Arts and Crafts',
+            image: '/images/art.jpg',
+            price: 130,
+            brand: 'US Art Supply',
+            rating:'2.0',
+            numReviews: 3,
+            description: 'The new copy machine of your dreams.'
+        },
+    ],
+};
+export default data;
