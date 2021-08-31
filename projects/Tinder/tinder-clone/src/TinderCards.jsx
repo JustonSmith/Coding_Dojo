8e31026e73c2b783013e13da7ea5d93fff1a4869
JustonSmith <75957152+JustonSmith@users.noreply.github.com>
2021-08-31 15:18:39 -0500

    tinder front and back end complete

--- projects/Tinder/tinder-clone/src/TinderCards.jsx
+++ projects/Tinder/tinder-clone/src/TinderCards.jsx
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TinderCard from "react-tinder-card"
 import "./TinderCards.css";
+import axios from './axios';
 
 function TinderCards() {
-const [people, setPeople] = useState([
-    {
-        name: "Elon Musk",
-        url: " https://s.yimg.com/ny/api/res/1.2/WrPAlgL0M.Np4p7wRVl8.Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTU0MA--/https://s.yimg.com/uu/api/res/1.2/JLI.F0NwAcr8_5nN1Lh1jA--~B/aD0xMDgwO3c9MTkyMDthcHBpZD15dGFjaHlvbg--/https://media.zenfs.com/en/gobankingrates_644/c7819312b1bc81ac7c2d68bd1593e05e"
-    },
-    {
-        name: "Jeff Bezos",
-        url: "https://i.guim.co.uk/img/media/332baa8afa46c6ce4696dcf8171f882514ff7f14/0_238_3290_1975/master/3290.jpg?width=445&quality=45&auto=format&fit=max&dpr=2&s=524528e7060d33cfd614ad9637cd2550"
-    },
-    {
-        name: "Mark Cuban",
-        url: " https://i.insider.com/5a3bf59d4aa6b509048b680f?width=804&format=jpeg"
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get('/tinder/cards')
+
+            setPeople(req.data);
     }
-]);
+    fetchData();
+    }, []);
+
+    console.log(people)
 
     const swiped = (direction, nameToDelete) => {
         console.log("removing " +nameToDelete);
@@ -39,7 +38,7 @@ const [people, setPeople] = useState([
                 >
 
                 <div
-                    style= {{ backgroundImage: `url(${person.url})` }}
+                    style= {{ backgroundImage: `url(${person.imgUrl})` }}
                     className="card"
                     >
                     <h3>{person.name}</h3>
